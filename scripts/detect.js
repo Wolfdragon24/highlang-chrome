1699f7d7f48d7cf197b5a5032a1f7ed3bb3f46c3
Wolfdragon24 <dragonbomber24@gmail.com>
2024-08-15 22:40:34 +1000

    Implement highlight detection, API settings functionality, and design aspects

--- scripts/detect.js
+++ scripts/detect.js
@@ -1,6 +1,10 @@
 let selection = [];
+let popup;
+
+window.onload = setup();
 
 document.addEventListener("mouseup", function (e) {
+    // Gets the location of the selected text when mouse hold is released
     const tempSelection = window.getSelection();
 
     if (tempSelection && tempSelection.rangeCount > 0) {
@@ -9,6 +13,9 @@ document.addEventListener("mouseup", function (e) {
 });
 
 document.addEventListener("mousemove", function (e) {
+    // Iterates through block of selected text
+    popup.style.display = "none";
+
     for(var i = 0 ; i < selection.length ; i++) {
         bounds = selection[i];
 
@@ -16,8 +23,60 @@ document.addEventListener("mousemove", function (e) {
         const y = e.clientY;
 
         if (x >= bounds.left && x <= bounds.right && y >= bounds.top && y <= bounds.bottom) {
-            console.log("Mouse is inside the selection");
+            const text = window.getSelection().toString();
+            fetchTranslation(text, bounds);
             return;
         }
     }
 });
+
+function setup() {
+    // Creates the popup element
+    popup = document.createElement("div");
+    popup.id = "popup";
+
+    document.body.appendChild(popup);
+};
+
+function updatePopup(text, bounds) {
+    // Updates popup with loaded text, makes it appear and moves to correct location
+    popup.innerHTML = text;
+    popup.style.display = "block";
+    popup.style.top = `${bounds.top - popup.offsetHeight * 1.1}px`;
+    popup.style.left = `${bounds.left}px`;
+}
+
+function fetchTranslation(text) {
+    // Fetches translation from API
+    chrome.storage.local.get(null, function(data) {
+        const apiURL = data.apiURL;
+        const apiKey = data.apiKey;
+
+        // Returns error if no API provided
+        if (apiURL === undefined) {
+            updatePopup("Error: No API URL has been set.", bounds);
+            return;
+        }
+
+        // Makes request for translation
+        fetch(apiURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify({
+                text: text
+            })
+        }).then(response => response.json()).then(data => {
+            // If successful, updates popup with translation
+            updatePopup(data.html, bounds);
+            return;
+        }).catch((error) => {
+            // Otherwise, returns error from request
+            console.error('Error:', error);
+            updatePopup(`Error: Failed to fetch translation: ${error}`);
+            return;
+        });
+    });
+}
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,12 +1,14 @@
 const apiRegex = /https?:\/\/[\d\w:\/\.]+/;
 
-document.onload(function() {
+document.onload = function pullData() {
   chrome.storage.local.get('apiURL', function(data) {
     if (data.apiURL !== undefined) {
       document.getElementById("translateAPI").value = data.apiURL;
     }
   });
-});
+};
+
+document.getElementById("saveAPI").addEventListener("click", saveAPI);
 
 function saveAPI() {
   const api = document.getElementById("translateAPI").value;
@@ -17,19 +19,32 @@ function saveAPI() {
   // Check if the inputted value is a valid URL
   if (api === '' || extracted === null) {
     result.innerText = "Error: Your API URL is invalid, please try again.";
+    result.style.display = "block";
     return;
   }
 
+  const key = document.getElementById("translateKey").value;
+
   // Check if the API URL has already been saved
-  chrome.storage.local.get('apiURL', function(data) {
+  chrome.storage.local.get(null, function(data) {
     if (data.apiURL !== undefined) {
-      result.innerText = "Error: This API URL has already been saved.";
+      if (data.apiKey === key) {
+        result.innerText = "Error: This API URL has already been saved.";
+        result.style.display = "block";
+        return;
+      }
+      
+      chrome.storage.local.set({apiKey: key}, function() {
+        result.innerText = "Success: Your API Key has been updated.";
+        result.style.display = "block";
+      });
       return;
     }
   });
 
   // Informs user of success
-  chrome.storage.local.set({apiURL: extracted[0]}, function() {
-    result.innerText = "Success: Your API URL has been saved.";
+  chrome.storage.local.set({apiURL: extracted[0], apiKey: key}, function() {
+    result.innerText = "Success: Your API URL and Key have been saved.";
+    result.style.display = "block";
   });
 }
